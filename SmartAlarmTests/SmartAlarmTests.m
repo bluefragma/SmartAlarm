129c226d01f67862e26a4834b24622224342995a
bluefragma <dkyang73@gmail.com>
2014-06-24 00:32:14 +0900

    Initial Commit

---
+++ SmartAlarmTests/SmartAlarmTests.m
@@ -0,0 +1,34 @@
+//
+//  SmartAlarmTests.m
+//  SmartAlarmTests
+//
+//  Created by 양동길 on 2014. 6. 24..
+//  Copyright (c) 2014년 양동길. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SmartAlarmTests : XCTestCase
+
+@end
+
+@implementation SmartAlarmTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
